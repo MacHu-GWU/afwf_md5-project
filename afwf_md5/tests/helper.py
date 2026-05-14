ea15e0a24b4475ce6372dcab754df21773fc9c93
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-05-14 19:43:23 -0400

    remove old code

--- afwf_md5/tests/helper.py
+++ afwf_md5/tests/helper.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from ..paths import dir_project_root, dir_htmlcov
+from ..paths import path_enum
 from ..vendor.pytest_cov_helper import run_cov_test as _run_cov_test
 
 
@@ -10,8 +10,8 @@ def run_cov_test(
     _run_cov_test(
         script=script,
         module=module,
-        root_dir=f"{dir_project_root}",
-        htmlcov_dir=f"{dir_htmlcov}",
+        root_dir=str(path_enum.dir_project_root),
+        htmlcov_dir=str(path_enum.dir_htmlcov),
         preview=preview,
         is_folder=is_folder,
     )
